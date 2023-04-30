b5799051ae274980ed16e3bd71a5a8d7c4519ff5
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-04-30 13:50:23 +0530

    Node Mern2

---
+++ node/http.js
@@ -0,0 +1,18 @@
+const http = require("http"); // IMPORT
+const data = require("./currencydata.json");
+
+const server = http.createServer((request, response) => {
+  if (request.url === "/") {
+    response.write("<h1>Currency Database</h1>");
+  } else if (request.url === "/currencies") {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.write(JSON.stringify(data));
+  } else {
+    response.writeHead(404);
+  }
+  response.end();
+});
+
+server.listen(8081, () => {
+  console.log("STARTED TO LISTEN ...");
+});
