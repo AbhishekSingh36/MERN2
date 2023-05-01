6187d4c7180beb886e7db4949fd345a674d8829e
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-01 13:54:01 +0530

    Node Mern2

--- node/http.js
+++ node/http.js
@@ -46,3 +46,6 @@ const server = http.createServer((request, response) => {
             response.writeHead(200, { "Content-Type": "application/json" });
             response.write(JSON.stringify(currenyData));
           }
+          else {
+            response.writeHead(404);
+          }
