e6c719d2113be6d73734c504ce3d308371f72e32
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-02 13:56:42 +0530

    Node Mern2

--- node/index.js
+++ node/index.js
@@ -1,3 +1,8 @@
 // const express = require("express");
 // const app = express();
 // const PORT = 8082;
+
+// app.get("/", (request, response) => {
+//   response.write("<h1> Hello from Express </h1>");
+//   response.end();
+// });
