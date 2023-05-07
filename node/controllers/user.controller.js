1fad3dcb8813250dd5de8e7590cdbafec623e0b9
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-07 20:00:42 +0530

    Node Mern2

--- node/controllers/user.controller.js
+++ node/controllers/user.controller.js
@@ -4,3 +4,13 @@ const allUsers = require('../usersData.json')
 const getUsers = (request, response) => {
     response.json(allUsers)
 }
+const getUsersWithUuid = (request,response) => {
+    const uuid = request.params.uuid;
+    const user = allUsers.data.find((u) => u.login.uuid === uuid)
+    if(user){
+        response.json(user)
+    }else{
+        
+        response.sendStatus(404)
+    }
+}
