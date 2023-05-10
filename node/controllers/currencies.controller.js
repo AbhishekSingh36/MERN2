11191ee3adbc7467437a56802e54dc7aba5d5504
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-10 20:02:28 +0530

    Node Mern2

--- node/controllers/currencies.controller.js
+++ node/controllers/currencies.controller.js
@@ -1,1 +1,12 @@
 const currencyJson = require('../currencydata.json')
+const getCurrencies = (request, response) => {
+    const minValue = request.query.min_value; //ask this in QnA
+    if (minValue) {
+      const data = currencyJson.data.filter(
+        (currency) => Number(currency.min_size) === Number(minValue)
+      );
+      response.json(data); // ask this why response.json QnA
+    } else {
+      response.json(currencyJson);
+    }
+  };
