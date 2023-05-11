a1314fba13a4d731bc02021e2f6bdcb1a44e57b3
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-11 20:03:33 +0530

    Node Mern2

--- node/controllers/currencies.controller.js
+++ node/controllers/currencies.controller.js
@@ -1,16 +1,18 @@
 const currencyJson = require('../currencydata.json')
+
 const getCurrencies = (request, response) => {
-    const minValue = request.query.min_value; //ask this in QnA
-    if (minValue) {
-      const data = currencyJson.data.filter(
-        (currency) => Number(currency.min_size) === Number(minValue)
-      );
-      response.json(data); // ask this why response.json QnA
-    } else {
-      response.json(currencyJson);
-    }
-  };
-  const getCurrencyFromSymbol = (request, response) => {
+  const minValue = request.query.min_value; //ask this in QnA
+  if (minValue) {
+    const data = currencyJson.data.filter(
+      (currency) => Number(currency.min_size) === Number(minValue)
+    );
+    response.json(data); // ask this why response.json QnA
+  } else {
+    response.json(currencyJson);
+  }
+};
+
+const getCurrencyFromSymbol = (request, response) => {
     const symbol = request.params.symbol
     //console.log(currency)
     const currencyData = currencyJson.data.find(
@@ -22,4 +24,5 @@ const getCurrencies = (request, response) => {
         response.sendStatus(404);
       }
 }
+
 module.exports = {getCurrencies, getCurrencyFromSymbol}
