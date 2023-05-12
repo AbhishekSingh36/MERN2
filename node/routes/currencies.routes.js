ec0f8de4901b57ed687b0f94ec1337f70f2deffa
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-13 00:33:16 +0530

    Update MERN2 Routes

--- node/routes/currencies.routes.js
+++ node/routes/currencies.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const x = require('..currencies.controller')
+const x = require('../controllers/currencies.controller')
 
 router.get('/currencies', x.getCurrencies)
 router.get('/currencies/:symbol', x.getCurrencyFromSymbol)
