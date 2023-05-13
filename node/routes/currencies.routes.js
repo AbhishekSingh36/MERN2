f1330a965b3b0cb2289cc8ba655f7f3f554edf8d
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-13 19:50:48 +0530

    Update MERN2 Routes

--- node/routes/currencies.routes.js
+++ node/routes/currencies.routes.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 const x = require('../controllers/currencies.controller')
+// prefix + /
+// currencies/currencies
+// router.get('/currencies', x.getCurrencies)
 
-router.get('/currencies', x.getCurrencies)
-router.get('/currencies/:symbol', x.getCurrencyFromSymbol)
+// // we came here using . currencies ---> currencies/currencies/symbol
+// router.get('/currencies/:symbol', x.getCurrencyFromSymbol)
+// --------- Solution ---------
+
+router.get('/', x.getCurrencies)
+router.get('/:symbol', x.getCurrencyFromSymbol)
 
 module.exports = router
