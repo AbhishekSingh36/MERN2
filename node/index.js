7785bd87e9a9cc2846cb389952028d9079a6212e
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-14 08:33:05 +0530

    Update MERN2

--- node/index.js
+++ node/index.js
@@ -1,25 +1,11 @@
-// ------------- Express --------------------
-
-// const express = require("express");
-// const app = express();
-// const PORT = 8082;
-
-// app.get("/", (request, response) => {
-//   response.write("<h1> Hello from Express </h1>");
-//   response.end();
-// });
-// app.listen(PORT, () => {
-//   console.log("Started to listen on PORT: ", PORT);
-// });
-
-//------------- 2 ------------
-
 const { request, response } = require('express');
 const express = require('express')
+//const usersControllers = require('./controllers/users.controllers');
+
+const currencyRoutes = require('./routes/currencies.routes')
+const usersRoutes = require('./routes/users.routes')
 const app = express();
 const PORT = 8082
-const usersControllers = require('./controllers/users.controllers')
-const currencyRoutes = require('./routes/currencies.routes')
 
 app.get('/', (request,response) => {
     // response.write('<h1>Hello from express</h1>')
@@ -30,10 +16,30 @@ app.get('/', (request,response) => {
 
 
 app.use('/currencies', currencyRoutes)
-app.get('/users', usersControllers.getUsers)
-app.get('/users/search', usersControllers.searchUsers)
-app.get('/users/:uuid', usersControllers.getUsersWithUuid )
+app.use('/users',usersRoutes )
 
+app.listen(PORT, () => {
+    console.log('Started to listen on PORT:', PORT)
+})
+const { request, response } = require('express');
+const express = require('express')
+//const usersControllers = require('./controllers/users.controllers');
+
+const currencyRoutes = require('./routes/currencies.routes')
+const usersRoutes = require('./routes/users.routes')
+const app = express();
+const PORT = 8082
+
+app.get('/', (request,response) => {
+    // response.write('<h1>Hello from express</h1>')
+    // response.end() 
+    // ------- Short--
+    response.send('<h1>Currency Database</h1>')
+})
+
+
+app.use('/currencies', currencyRoutes)
+app.use('/users',usersRoutes )
 
 app.listen(PORT, () => {
     console.log('Started to listen on PORT:', PORT)
