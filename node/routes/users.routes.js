9f01a7826011d1088fee8eba626c693afcdb41b4
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-14 08:34:08 +0530

    Update MERN2

--- node/routes/users.routes.js
+++ node/routes/users.routes.js
@@ -1,2 +1,5 @@
 const router = require('express').Router();
 const usersControllers = require('../controllers/users.controllers');
+const { route } = require('./currencies.routes');
+
+router.get('/', usersControllers.getUsers);
