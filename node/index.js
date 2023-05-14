0c90f5da47c3a2464e96f13ef559351c8946f663
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-14 08:34:45 +0530

    Update MERN2

--- node/index.js
+++ node/index.js
@@ -21,26 +21,3 @@ app.use('/users',usersRoutes )
 app.listen(PORT, () => {
     console.log('Started to listen on PORT:', PORT)
 })
-const { request, response } = require('express');
-const express = require('express')
-//const usersControllers = require('./controllers/users.controllers');
-
-const currencyRoutes = require('./routes/currencies.routes')
-const usersRoutes = require('./routes/users.routes')
-const app = express();
-const PORT = 8082
-
-app.get('/', (request,response) => {
-    // response.write('<h1>Hello from express</h1>')
-    // response.end() 
-    // ------- Short--
-    response.send('<h1>Currency Database</h1>')
-})
-
-
-app.use('/currencies', currencyRoutes)
-app.use('/users',usersRoutes )
-
-app.listen(PORT, () => {
-    console.log('Started to listen on PORT:', PORT)
-})
