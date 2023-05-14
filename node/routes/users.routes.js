5cd6db7a2810fdbc863e9b5b2cd28f5f4472ce0f
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-14 08:34:23 +0530

    Update MERN2

--- node/routes/users.routes.js
+++ node/routes/users.routes.js
@@ -3,3 +3,7 @@ const usersControllers = require('../controllers/users.controllers');
 const { route } = require('./currencies.routes');
 
 router.get('/', usersControllers.getUsers);
+router.get('/search', usersControllers.searchUsers);
+router.get('/:uuid', usersControllers.getUsersWithUuid)
+
+module.exports = router
