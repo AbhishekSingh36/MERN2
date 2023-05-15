9a7bd228615a86bd74309b2d5a385f0f362e9898
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-15 23:45:55 +0530

    Update MERN2

--- node/validators/users.validators.js
+++ node/validators/users.validators.js
@@ -2,5 +2,7 @@ const Joi = require('joi')
 
 const schema = Joi.object().keys({
     //this is the schema
-    
+    age: Joi.number().integer().min(0).max(100),
+    gender: Joi.string().valid('male','female')
 })
+.or('age', 'gender') 
