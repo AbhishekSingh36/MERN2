b65a02d68b66729ab6c1843b206b070f0ea15010
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-15 07:21:18 +0530

    Update MERN2

--- node/controllers/user.controller.js
+++ node/controllers/user.controller.js
@@ -1,61 +1,67 @@
-
-const allUsers = require('../usersData.json')
+const allUsers = require("../usersData.json");
 
 const getUsers = (request, response) => {
-    response.json(allUsers)
-}
+  response.json(allUsers);
+};
 
+const getUsersWithUuid = (request, response) => {
+  const uuid = request.params.uuid;
+  const user = allUsers.data.find((u) => u.login.uuid === uuid);
+  if (user) {
+    response.json(user);
+  } else {
+    response.sendStatus(404);
+  }
+};
 
-const getUsersWithUuid = (request,response) => {
-    const uuid = request.params.uuid;
-    const user = allUsers.data.find((u) => u.login.uuid === uuid)
-    if(user){
-        response.json(user)
-    }else{
-        
-        response.sendStatus(404)
+const searchUsers = (request, response) => {
+  const { gender, age } = request.query;
+  if (gender && age) {
+    response.json(
+      allUsers.data.filter(
+        (u) => u.gender === gender && Number(u.dob.age) === Number(age)
+      )
+    );
+  } else if (!gender && !age) {
+    return response
+      .status(422)
+      .json({ message: "Please give gender or age or both" });
+  } else if (age) {
+    if (!Number(age)) {
+      return response.status(422).json({ message: "Age should be a number" });
+    } else if (Number(age) < 0 || Number(age) >= 100) {
+      return response
+        .status(422)
+        .json({ message: "Age should be between 0-100" });
     }
-}
-
-const searchUsers = (request,response) => {
-    const {gender, age} = request.query 
-    if(gender && age){
-        response.json(
-            allUsers.data.filter(u => u.gender === gender && Number(u.dob.age) === Number(age))
-        )
-    } else if(!gender && !age){
-        return response
-            .status(422)
-            .json({message: 'Please give gender or age or both'})
-    } else if(age){
-        if(!Number(age)){
-            return response
-            .status(422)
-            .json({message: 'Age should be a number'})
-        }
-        else if(Number(age) < 0 || Number(age) >= 100){
-            return response
-            .status(422)
-            .json({message: 'Age should be between 0-100'})
-        }
-    }
-    else if(gender){
-        
-        response.json(
-            allUsers.data.filter(
-                (u) => u.gender === gender
-            )
-        )
-    } else if(age){
-        
-        response.json(
-            allUsers.data.filter( u => Number(u.dob.age) === Number(age))
-        )
+  } else if (gender) {
+     if (!["male", "female"].includes(gender)) {
+      return response
+        .status(422)
+        .json({ message: "Gender should be male or female" });
     } else {
-        
-        response.sendStatus(404)
+      response.json(
+          allUsers.data.filter(
+              (u) => u.gender === gender
+          )
+      )
     }
-}
+
+    // response.json(
+    //     allUsers.data.filter(
+    //         (u) => u.gender === gender
+    //     )
+    // )
+  } else if (age) {
+    response.json(
+      allUsers.data.filter((u) => Number(u.dob.age) === Number(age))
+    );
+  } else {
+    response.sendStatus(404);
+  }
+};
 module.exports = {
-    getUsers,searchUsers, getUsersWithUuid
-}
+  getUsers,
+  searchUsers,
+  getUsersWithUuid,
+};
