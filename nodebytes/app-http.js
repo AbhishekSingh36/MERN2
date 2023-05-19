0079b77cad025038d6dd8afeb06cb6cb683d35db
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-19 05:30:19 +0530

    Node Bytes

--- nodebytes/app-http.js
+++ nodebytes/app-http.js
@@ -1,3 +1,17 @@
 const http = require('http');
 const port = 8081;
 let todoList = ["Complete Node Byte", "Play Cricket"];
+
+http.createServer((request, response) => {
+    const { method, url } = request;
+
+    if (url == "/todos") {
+
+        /* Get all TODOS:   
+        ** curl -v http://localhost:8081/todos
+        */
+        if (method === "GET") {
+            response.writeHead(200);
+            response.write(todoList.toString())
+
+        }
