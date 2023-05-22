e87da8a23c9b3cac5d6d5dcace0b3c6d9093be25
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-22 06:44:45 +0530

    Node Bytes

--- nodebytes/app.js
+++ nodebytes/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
-
+const port = 8081;
 
 app.get("/todos", (request,response) => {
     response.send(todoList);
+});
+
+app.listen(port, function(){
+    console.log(`Nodejs server started on port ${port}`)
 });
