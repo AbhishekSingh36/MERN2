f990934ae66fd5c82c39f7fcbb805ab5bc4d9c1e
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-27 23:36:24 +0530

    MERN2

--- node/routes/users.routes.js
+++ node/routes/users.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const usersControllers = require('../controllers/users.controllers');
 const { route } = require('./currencies.routes');
+const {validateSearchQuery} = require('../middlewares/validateSearchQuery')
 
 router.get('/', usersControllers.getUsers);
 router.get('/search', usersControllers.searchUsers);
