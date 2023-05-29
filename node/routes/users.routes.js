57ccd616e00e896e2d2972d08a2fab38c1ef6c97
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-05-29 07:33:10 +0530

    MERN2

--- node/routes/users.routes.js
+++ node/routes/users.routes.js
@@ -4,7 +4,7 @@ const { route } = require('./currencies.routes');
 const {validateSearchQuery} = require('../middlewares/validateSearchQuery')
 
 router.get('/', usersControllers.getUsers);
-router.get('/search', usersControllers.searchUsers);
+router.get('/search', validateSearchQuery, usersControllers.searchUsers);
 router.get('/:uuid', usersControllers.getUsersWithUuid)
 
 module.exports = router
