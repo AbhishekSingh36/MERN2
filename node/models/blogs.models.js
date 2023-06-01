4c2eb41b5023973fcc2a8b9bc2a3a6fce422f063
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-06-01 10:47:46 +0530

    MERN2

--- node/models/blogs.models.js
+++ node/models/blogs.models.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
+// Schema tells you what the keys are and what valuses should be --- Models will give you the API to do it
+ const blogSchema = new mongoose.Schema({
     title: String,
     author: [String],
     content: String,
     publishedAt: Date
  })
+
+ // To use and validate data we use mongoose model
+ const model = mongoose.model('blogs', blogSchema)
