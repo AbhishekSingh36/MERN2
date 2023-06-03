84a09422777a6777f55353dca969417138e851bb
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-06-03 22:28:03 +0530

    MERN2

--- node/controllers/blogs.controllers.js
+++ node/controllers/blogs.controllers.js
@@ -1,12 +1,12 @@
 const { response } = require('express')
 const Blogs = require('../models/blogs.models')
 
-const createNewBlogs = (Request, Response) => {
+const createNewBlog = (Request, Response) => {
     const document = new Blogs({title: 'First Blog'})
     console.log(document)
     response.sendStatus(200)
 }
 
 module.exports = {
-    createNewBlogs
+    createNewBlog
 }
