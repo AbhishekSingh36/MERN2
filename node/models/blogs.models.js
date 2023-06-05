d0225cb08b8f0719296338fdd2c905a25a51d6a3
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-06-05 21:24:04 +0530

    MERN2

--- node/models/blogs.models.js
+++ node/models/blogs.models.js
@@ -11,8 +11,8 @@ const mongoose = require('mongoose')
 const blogSchema = new mongoose.Schema({
    title: {type: String, required: true, unique: true},
    author: {type: [String]},
-   content: {type: String},
-   publishedAt: Date
+   content: {type: String, default: ''},
+   publishedAt: {type:Date, default: null},
 })
 
  // To use and validate data we use mongoose model
