d958558cf48f9ca72d0ff91011a019536c08b391
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-06-07 15:20:25 +0530

    MERN2

--- node/routes/blogs.routes.js
+++ node/routes/blogs.routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const blogsController = require('../controllers/blogs.controllers')
 
-router.get('/new', blogsController.createNewBlog)
-
+//router.get('/new', blogsController.createNewBlog)
+router.post('/new', blogsController.createNewBlog)
 
 module.exports = router
