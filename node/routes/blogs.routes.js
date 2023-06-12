0af89f87fe6df1aef6d267ad5796ac49a18781b1
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-06-12 15:19:13 +0530

    MERN2

--- node/routes/blogs.routes.js
+++ node/routes/blogs.routes.js
@@ -4,6 +4,7 @@ const blogsController = require('../controllers/blogs.controllers')
 //router.get('/new', blogsController.createNewBlog)
 router.get('/', blogsController.getAllBlogs)
 router.delete('/:id', blogsController.deleteBlogWithid)
+router.patch('/:id', blogsController.updateBlogWithId)
 router.post('/new', blogsController.createNewBlog)
 
 module.exports = router
