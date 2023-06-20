6f30e79d7422ec20bef79e5be7618f054b7ed3cb
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-06-20 12:58:17 +0530

    MERN2

--- node/services/blog.service.js
+++ node/services/blog.service.js
@@ -3,11 +3,12 @@ const Blogs = require('../models/blogs.models')
 const findAllBlogs = async () => {
     return await Blogs.find({})
 }
-const createNewBlog = async () => {
-    
+const createNewBlog = async (data) => {
+    const document = new Blogs(data) // takes an input
+    await document.save()
 }
 
 module.exports = {
     findAllBlogs,
-    createNewBlog
+    createNewBlog,
 }
