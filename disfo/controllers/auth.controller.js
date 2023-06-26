754e92948fb470887e9395a248dcde0026847da7
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-06-27 01:05:23 +0530

    MERN2

--- disfo/controllers/auth.controller.js
+++ disfo/controllers/auth.controller.js
@@ -1,5 +1,14 @@
-const postSignup = (request,response) => {
+const AuthService = require('../services/auth.service')
+const AuthServiceInstance = new AuthService()
 
+const postSignup = (request,response) => {
+    try {
+        const data = request.body 
+        const result =  await AuthServiceInstance.signup(data)
+        response.json(result)
+    } catch (err) {
+        response.status(500).json(err)
+    }
 }
 
 module.exports = {
