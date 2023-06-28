2b7f42a15281e77094cd8546e009bbb708aa62f7
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-06-28 15:18:40 +0530

    MERN2

--- disfo/controllers/auth.controller.js
+++ disfo/controllers/auth.controller.js
@@ -1,10 +1,10 @@
 const AuthService = require('../services/auth.service')
-const AuthServiceInstance = new AuthService()
+const AuthServiceInstance = new AuthService() // ask in doubt
 
 const postSignup = async(request,response) => {
     try {
-        const data = request.body 
-        const result =  await AuthServiceInstance.signUp(data)
+        const data = request.body // email username password
+        const result =  await AuthServiceInstance.signup(data)
         response.json(result)
     } catch (err) {
         response.status(500).json(err)
