010d00df082dc7665e4c6541e65ce55dc44f2f20
AbhishekSingh36 <abhisheksingh.nyc1@gmail.com>
2023-08-08 15:55:53 +0530

    disfo update

--- disfo/services/auth.service.js
+++ disfo/services/auth.service.js
@@ -1,6 +1,12 @@
+const UserService = require('./user.service')
+const UserServiceInstance = new UserService();
+
 class AuthService {
-    signUp = async() => {
+    signup = async(user) => {
         console.log(user)
+        const result = await UserServiceInstance.register(user)
+        return result
+
     }
 }
 
