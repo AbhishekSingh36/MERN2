5ab9f6c1e98f0e77fb462900ddb457b793be5700
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-08-13 01:48:50 +0530

    disfo update

--- disfo/controllers/auth.controller.js
+++ disfo/controllers/auth.controller.js
@@ -11,6 +11,24 @@ const postSignup = async(request,response) => {
     }
 }
 
+const postLogin = async(request,response) => {
+    try {
+        const {username, password} = request.body // email username password
+        const result =  await AuthServiceInstance.login({username, password})
+        if (result.isLoggedIn){
+            response.json(result);
+        }else{
+            response.status(403).json({message: "Incorrect Credentials"})
+        }
+        response.json(result)
+    } catch (err) {
+        response.status(500).json(err)
+    }
+}
+
+
+
 module.exports = {
-    postSignup
+    postSignup,
+    postLogin
 }
