98af2627862dcb7ad2631e24a713d33999fa7992
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-08-13 01:49:15 +0530

    disfo update

--- disfo/routes/auth.routes.js
+++ disfo/routes/auth.routes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
-const { postSignup} = require("../controllers/auth.controller")
+const { postSignup,postLogin} = require("../controllers/auth.controller")
 const { validateSchema } = require("../middlewares/validate.middleware");
 const { userValidationSchema } = require("../validations/user.validator");
 
 const middleware = validateSchema(userValidationSchema)
 
 router.post("/signup",middleware,postSignup)
+router.post('/login',postLogin)
 
 module.exports = router 
