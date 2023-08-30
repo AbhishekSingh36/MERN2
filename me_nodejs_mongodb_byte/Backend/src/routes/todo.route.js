7be139f46a8281a6901845172668a55ac0727027
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-08-30 11:43:10 +0530

    Nodeandmongo

--- me_nodejs_mongodb_byte/Backend/src/routes/todo.route.js
+++ me_nodejs_mongodb_byte/Backend/src/routes/todo.route.js
@@ -9,96 +9,14 @@ const Todos = require("../../models/todo.model");
  * Get todos with their "startDate" b/w startDateMin and startDateMax
  * curl http://localhost:8082/v1/todos?startDateMin=2020-11-04&startDateMax=2020-12-30
  * 
+ * Get todos with "pending" field as "true"
+ * curl http://localhost:8082/v1/todos?pending=true
  */
-
- router.get("/", async (req, res) => {
-  //  console.log(
-  //    `URL:  /v1/todos${req.url == "/" ? "" : req.url}, Method:  ${req.method}, Timestamp: ${new Date()}`
-  //  );
-   const allTodos = await Todos.find({});
-   res.send(allTodos);
-});
-
-router.post("/", async (req, res) => {
-  //  console.log(
-  //    `URL:  /v1/todos${req.url == "/" ? "" : req.url}, Method:  ${req.method}, Timestamp: ${new Date()}`
-  //  );
-   console.log("Request body: ", req.body);
-   //console.log("Response Body undefined as of Now:", res.body)
- 
-   let newTodo = {
-     name: req.body.name,
-     startDate: req.body.startDate,
-     endDate: req.body.endDate,
-   };
- 
-   const newlyCreated = await Todos.create(newTodo);
-   res.status(201).send(newlyCreated)
-
-   
-  Todos.create(newTodo, (err, newlyCreated) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send();
-    } else {
-      console.log("New todo item: ", newlyCreated);
-      res.status(201).send(newlyCreated);
-    }
-  });
- });
-
- router.put("/", async(req,res)=> {
-  console.log("Request Body", req.body);
-  // console.log(`URL:  /v1/todos${req.url == "/" ? "" : req.url}, Method:  ${req.method}, Timestamp: ${new Date()}`)
-
-  const idToUpdate = req.body._id;
-  const updatedTodo = {
-    name: req.body.anem,
-    startDate: req.body.startDate,
-    endDate: req.body.endDate,
-    pending: req.body.pending
-  }
-
-  Todos.findByIdAndUpdate(idToUpdate,updatedTodo, (err,doc) => {
-    if(err) {
-      console.log(err)
-      res.status(500).send()
-    } else if(doc == null){
-      res.status(400).send({ error: "Resource not Found"})
-    } else{
-      res.status(204).send()
-    }
-  })
- })
-
- router.delete("/:id", (req, res) => {
-  const IdToDelete = req.params.id;
-  console.log("IdToDelete", idToDelete)
-  console.log("Deleted Body", req.body)
-  console.log(
-    `URL:  /v1/todos${req.url == "/" ? "" : req.url}, Method:  ${req.method}, Timestamp: ${new Date()}`
-  );
-  Todos.findByIdAndDelete(IdToDelete, (err, result) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send();
-    } else {
-      res
-        .status(204)
-        .send();
-    }
-  });
-});
-
-
 router.get("/", async (req, res) => {
-  // console.log(
-  //   `URL:  /v1/todos${req.url == "/" ? "" : req.url}, Method:  ${req.method}, Timestamp: ${new Date()}`
-  // );
   if (req.query.startDateMax && req.query.startDateMin) {
-
     let startDateMax = new Date(req.query.startDateMax);
     startDateMax.setTime(startDateMax.getTime());
+
     let startDateMin = new Date(req.query.startDateMin);
     startDateMin.setTime(startDateMin.getTime());
 
@@ -117,6 +35,15 @@ router.get("/", async (req, res) => {
         }
       }
     );
+  } else if (req.query.pending) {
+    Todos.find({ pending: req.query.pending }, (err, allTodos) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send();
+      } else {
+        res.send(allTodos);
+      }
+    });
   } else {
     Todos.find({}, (err, allTodos) => {
       if (err) {
@@ -129,28 +56,71 @@ router.get("/", async (req, res) => {
   }
 });
 
-
 /**
  * Add a TODO to the list
  * curl -X POST http://localhost:8082/v1/todos \
     -d '{"name": "Learn Nodejs by doing","startDate": "2021-01-07","endDate": "2021-01-09"}' \
     -H 'Content-Type: application/json'
 */
-// router.post("/", async (req, res) => {
+router.post("/", async (req, res) => {
+  console.log("Request body: ", req.body);
 
-// });
+  if (!req.is("application/json")) {
+    res.status(415);
+    res.send({ error: "Received non-JSON data" });
+  }
+
+  let newTodo = {
+    name: req.body.name,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+  };
+
+  Todos.create(newTodo, (err, newlyCreated) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+    } else {
+      console.log("New todo item: ", newlyCreated);
+      res.status(201).send(newlyCreated);
+    }
+  });
+});
 
 /**
  * Update an existing TODO
  * curl -v -X PUT http://localhost:8082/v1/todos \
-    -d '{"_id": "<id-value>", "name": "Play tennis","startDate": "2021-01-07","endDate": "2021-01-09"}' \
+    -d '{"_id": "<id-value"", "name": "Play tennis","startDate": "2021-01-07","endDate": "2021-01-09"}' \
     -H 'Content-Type: application/json'
  * 
  * Nb: You'll need to change the "id" value to that of one of your todo items
 */
-// router.put("/", (req, res) => {
+router.put("/", (req, res) => {
+  console.log("Request body: ", req.body);
 
-// });
+  if (!req.is("application/json")) {
+    res.status(415).send({ error: "Received non-JSON data" });
+  }
+
+  const idToUpdate = req.body._id;
+  const updatedTodo = {
+    name: req.body.name,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+    pending: req.body.pending,
+  };
+
+  Todos.findByIdAndUpdate(idToUpdate, updatedTodo, (err, doc) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+    } else if (doc == null) {
+      res.status(400).send({ error: "Resource not found" });
+    } else {
+      res.status(204).send();
+    }
+  });
+});
 
 /**
  * Delete a TODO from the list
@@ -158,10 +128,198 @@ router.get("/", async (req, res) => {
  *
  * Nb: You'll need to change "<id-value>" to the "id" value of one of your todo items
  */
-// router.delete("/:id", (req, res) => {
+router.delete("/:id", (req, res) => {
+  const IdToDelete = req.params.id;
 
+  Todos.findByIdAndDelete(IdToDelete, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+    } else {
+      res.status(204).send();
+    }
+  });
+});
+
+/**
+ * Logs request metadata to terminal
+ * @param {Request} req 
+ */
+const logMetadata = (req) => {
+  console.log(
+    `URL:  /v1/todos${req.url == "/" ? "" : req.url}, Method:  ${
+      req.method
+    }, Timestamp: ${new Date()}`
+  );
+};
+
+module.exports = router;
+
+
+// const express = require("express");
+// const router = express.Router();
+// const Todos = require("../../models/todo.model");
+
+// /**
+//  * Get all TODOS:
+//  * curl http://localhost:8082/v1/todos
+//  *
+//  * Get todos with their "startDate" b/w startDateMin and startDateMax
+//  * curl http://localhost:8082/v1/todos?startDateMin=2020-11-04&startDateMax=2020-12-30
+//  * 
+//  */
+
+//  router.get("/", async (req, res) => {
+//   //  console.log(
+//   //    `URL:  /v1/todos${req.url == "/" ? "" : req.url}, Method:  ${req.method}, Timestamp: ${new Date()}`
+//   //  );
+//    const allTodos = await Todos.find({});
+//    res.send(allTodos);
+// });
+
+// router.post("/", async (req, res) => {
+//   //  console.log(
+//   //    `URL:  /v1/todos${req.url == "/" ? "" : req.url}, Method:  ${req.method}, Timestamp: ${new Date()}`
+//   //  );
+//    console.log("Request body: ", req.body);
+//    //console.log("Response Body undefined as of Now:", res.body)
+ 
+//    let newTodo = {
+//      name: req.body.name,
+//      startDate: req.body.startDate,
+//      endDate: req.body.endDate,
+//    };
+ 
+//    const newlyCreated = await Todos.create(newTodo);
+//    res.status(201).send(newlyCreated)
+
+   
+//   Todos.create(newTodo, (err, newlyCreated) => {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).send();
+//     } else {
+//       console.log("New todo item: ", newlyCreated);
+//       res.status(201).send(newlyCreated);
+//     }
+//   });
+//  });
+
+//  router.put("/", async(req,res)=> {
+//   console.log("Request Body", req.body);
+//   // console.log(`URL:  /v1/todos${req.url == "/" ? "" : req.url}, Method:  ${req.method}, Timestamp: ${new Date()}`)
+
+//   const idToUpdate = req.body._id;
+//   const updatedTodo = {
+//     name: req.body.anem,
+//     startDate: req.body.startDate,
+//     endDate: req.body.endDate,
+//     pending: req.body.pending
+//   }
+
+//   Todos.findByIdAndUpdate(idToUpdate,updatedTodo, (err,doc) => {
+//     if(err) {
+//       console.log(err)
+//       res.status(500).send()
+//     } else if(doc == null){
+//       res.status(400).send({ error: "Resource not Found"})
+//     } else{
+//       res.status(204).send()
+//     }
+//   })
+//  })
+
+//  router.delete("/:id", (req, res) => {
+//   const IdToDelete = req.params.id;
+//   console.log("IdToDelete", idToDelete)
+//   console.log("Deleted Body", req.body)
+//   console.log(
+//     `URL:  /v1/todos${req.url == "/" ? "" : req.url}, Method:  ${req.method}, Timestamp: ${new Date()}`
+//   );
+//   Todos.findByIdAndDelete(IdToDelete, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).send();
+//     } else {
+//       res
+//         .status(204)
+//         .send();
+//     }
+//   });
 // });
 
 
+// router.get("/", async (req, res) => {
+//   // console.log(
+//   //   `URL:  /v1/todos${req.url == "/" ? "" : req.url}, Method:  ${req.method}, Timestamp: ${new Date()}`
+//   // );
+//   if (req.query.startDateMax && req.query.startDateMin) {
 
-module.exports = router;
+//     let startDateMax = new Date(req.query.startDateMax);
+//     startDateMax.setTime(startDateMax.getTime());
+//     let startDateMin = new Date(req.query.startDateMin);
+//     startDateMin.setTime(startDateMin.getTime());
+
+//     Todos.find(
+//       {
+//         startDate: {
+//           $lte: startDateMax,
+//           $gte: startDateMin,
+//         },
+//       },
+//       (err, allTodos) => {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           res.send(allTodos);
+//         }
+//       }
+//     );
+//   } else {
+//     Todos.find({}, (err, allTodos) => {
+//       if (err) {
+//         console.log(err);
+//         res.status(500).send();
+//       } else {
+//         res.send(allTodos);
+//       }
+//     });
+//   }
+// });
+
+
+// /**
+//  * Add a TODO to the list
+//  * curl -X POST http://localhost:8082/v1/todos \
+//     -d '{"name": "Learn Nodejs by doing","startDate": "2021-01-07","endDate": "2021-01-09"}' \
+//     -H 'Content-Type: application/json'
+// */
+// // router.post("/", async (req, res) => {
+
+// // });
+
+// /**
+//  * Update an existing TODO
+//  * curl -v -X PUT http://localhost:8082/v1/todos \
+//     -d '{"_id": "<id-value>", "name": "Play tennis","startDate": "2021-01-07","endDate": "2021-01-09"}' \
+//     -H 'Content-Type: application/json'
+//  * 
+//  * Nb: You'll need to change the "id" value to that of one of your todo items
+// */
+// // router.put("/", (req, res) => {
+
+// // });
+
+// /**
+//  * Delete a TODO from the list
+//  * curl -v -X "DELETE" http://localhost:8082/v1/todos/<id-value>
+//  *
+//  * Nb: You'll need to change "<id-value>" to the "id" value of one of your todo items
+//  */
+// // router.delete("/:id", (req, res) => {
+
+// // });
+
+
+
+// module.exports = router;
